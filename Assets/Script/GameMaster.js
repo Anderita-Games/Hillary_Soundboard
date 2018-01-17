b3422ecb995b2dceabdbab77788bfa1a35357f86
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-16 19:45:43 -0500

    Added App
    This is the first version of Hillary Soundboard that GitHub has ever seen!

---
+++ Assets/Script/GameMaster.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function MainMenu() {
+	Application.LoadLevel("MainMenu");
+}
+
+function MainMenu2() {
+	Application.LoadLevel("MainMenu2");
+}
+
+function MainMenu3() {
+	Application.LoadLevel("MainMenu3");
+}
